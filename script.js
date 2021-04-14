c687228c4947be6f0028d0d380a786ccf34cd7c5
Ryan Graves <rmgraves@gmail.com>
2021-04-14 20:02:47 +0000

    Final project, with changes to CSS, HTML and JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years =  document.getElementById("years").value;    
+
+    //Interest is the principal amount times interest rate
+    var interest = principal * years * (rate / 100);
+
+    //Get the current year and add no. of years to it
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //If we enter zero or negative numbers alert
+    if(principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus(); //Set focus to the principal input
+        return;
+    }
+
+    //Result formatted with html
+    var result = "If you deposit <mark>" + principal + "</mark>,<br/>" +
+                 "at an interest rate of <mark>" + rate + "</mark>.<br/>" +
+                 "You will receive an amount of <mark>" + interest + "</mark>,<br/>" +
+                 "in the year <mark>" + year + "</mark>";
     
+    //Show the result below the compute button
+    document.getElementById("result").innerHTML = result;    
 }
         
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
